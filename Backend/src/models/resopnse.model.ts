2ecb35b388d4216947aec95f91ebc4b7cd24628f
Subham777-max <subhamray865@gmail.com>
2026-04-09 09:21:08 +0530

    response model is created

---
+++ Backend/src/models/resopnse.model.ts
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+
+const responseSchema = new mongoose.Schema({
+    problem:{
+        type: String,
+        default: ""
+    },
+    solution_1:{
+        type: String,
+        default: ""
+    },
+    solution_2:{
+        type: String,
+        default: ""
+    },
+    judge:{
+        solution_1_score:{
+            type: Number,
+            default: 0
+        },
+        solution_2_score:{
+            type: Number,
+            default: 0
+        },
+        solution_1_reasoning:{
+            type: String,
+            default: ""
+        },
+        solution_2_reasoning:{
+            type: String,
+            default: ""
+        }
+    }
+})
+
+const responseModel = mongoose.model("Response", responseSchema);
+
+export default responseModel;
