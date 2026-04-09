6df810753a5c53844f595b60b0e7caf7ca966068
Subham777-max <subhamray865@gmail.com>
2026-04-09 09:20:40 +0530

    created the mongouri config

--- Backend/src/config/config.ts
+++ Backend/src/config/config.ts
@@ -7,12 +7,14 @@ type CONFIG = {
     readonly GOOGLE_API_KEY: string;
     readonly MISTRAL_API_KEY: string;
     readonly COHERE_API_KEY: string;
+    readonly MONGO_URI: string;
 }
 
 const configs: CONFIG = {
     GOOGLE_API_KEY: process.env.GOOGLE_API_KEY || "",
     MISTRAL_API_KEY: process.env.MISTRAL_API_KEY || "",
     COHERE_API_KEY: process.env.COHERE_API_KEY || "",
+    MONGO_URI: process.env.MONGO_URI || ""
 }
 
 export default configs;
