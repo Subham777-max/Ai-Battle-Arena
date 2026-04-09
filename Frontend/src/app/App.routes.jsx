40f982652ba594c9d35e0c4ebeb1908e13ea97b1
Subham777-max <subhamray865@gmail.com>
2026-04-09 10:59:36 +0530

    added the new pages routes

--- Frontend/src/app/App.routes.jsx
+++ Frontend/src/app/App.routes.jsx
@@ -1,12 +1,16 @@
 import { Routes, Route } from 'react-router-dom';
 import ChatPage from '../features/Chat/pages/ChatPage';
 import HistoryPage from '../features/Chat/pages/HistoryPage';
+import LeaderboardPage from '../features/Chat/pages/LeaderboardPage';
+import ModelBenchPage from '../features/Chat/pages/ModelBenchPage';
 
 const AppRoutes = ({ onMenuClick }) => {
   return (
     <Routes>
       <Route path="/" element={<ChatPage onMenuClick={onMenuClick} />} />
       <Route path="/history" element={<HistoryPage onMenuClick={onMenuClick} />} />
+      <Route path="/leaderboard" element={<LeaderboardPage onMenuClick={onMenuClick} />} />
+      <Route path="/models" element={<ModelBenchPage onMenuClick={onMenuClick} />} />
     </Routes>
   );
 };
