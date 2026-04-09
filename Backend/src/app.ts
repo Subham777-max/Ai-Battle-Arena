6808b19f9340af86e5dd737a276988ab7c0bb4b3
Subham777-max <subhamray865@gmail.com>
2026-04-09 09:28:29 +0530

    removed the unusecode

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -1,5 +1,4 @@
 import express from "express";
-import runGraph from "./services/graph.ai.service.js";
 import aiRoutes from "./routes/ai.routes.js";
 const app = express();
 
@@ -13,8 +12,5 @@ app.get("/health", (req, res) => {
   res.status(200).json({ status: "ok" });
 });
 
-// app.post("/use-graph", async (req, res) => {
-//   const result = await runGraph("write an factorial function in javascript");
-//   res.status(200).json(result);
-// });
+
 export default app;
