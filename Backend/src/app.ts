4bd259f6cc4edb8dfe565d2bb41835bbda08ab6d
Subham777-max <subhamray865@gmail.com>
2026-04-09 09:31:19 +0530

    cors added

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -1,9 +1,14 @@
 import express from "express";
 import aiRoutes from "./routes/ai.routes.js";
+import cors from "cors";
 const app = express();
 
 app.use(express.json());
-
+app.use(cors({
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"],
+    credentials: true
+}));
 
 app.use("/api/ai", aiRoutes);
 
