a877954f4df7bd22fb009d0dd564748014e03efc
Subham777-max <subhamray865@gmail.com>
2026-04-09 09:20:50 +0530

    small update

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -1,15 +1,20 @@
 import express from "express";
 import runGraph from "./services/graph.ai.service.js";
+import aiRoutes from "./routes/ai.routes.js";
 const app = express();
 
 app.use(express.json());
+
+
+app.use("/api/ai", aiRoutes);
+
 
 app.get("/health", (req, res) => {
   res.status(200).json({ status: "ok" });
 });
 
-app.post("/use-graph", async (req, res) => {
-  const result = await runGraph("write an factorial function in javascript");
-  res.status(200).json(result);
-});
+// app.post("/use-graph", async (req, res) => {
+//   const result = await runGraph("write an factorial function in javascript");
+//   res.status(200).json(result);
+// });
 export default app;
